f0ed1f7b5840cacaad3f80786195b43d720bc2b3
sshhane <g00332592@gmit.ie>
2019-01-27 21:36:29 +0000

    finished max_2_sum(arr)

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -17,9 +17,20 @@ raise 'sum([]) != 0' unless sum([]) == 0
 raise 'sum([1]) != 1' unless sum([1]) == 1
 raise 'sum([1,2,3]) != 6' unless sum([1,2,3]) == 6
 
+# Define a method max_2_sum(array) which takes an array of integers as an argument
+# and returns the sum of its two largest elements. For an empty array it should return zero.
+# For an array with just one element, it should return that element. Run associated tests via:
+# $ rspec -e '#max_2_sum' spec/part1_spec.rb
+
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  arr.max(2).reduce(0,:+)
 end
+
+# my test cases
+raise 'max_2_sumum([]) != 0' unless max_2_sum([]) == 0
+raise 'max_2_sum([9]) != 9' unless max_2_sum([9]) == 9
+raise 'max_2_sum([1,2,3]) != 5' unless max_2_sum([1,2,3]) == 5
+raise 'max_2_sum([5,4,2,10]) != 15' unless max_2_sum([5,4,2,10]) == 15
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
