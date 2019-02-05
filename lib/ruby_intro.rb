b0e8ef55ad8c05cb6b46636734bf10876a2a4785
sshhane <g00332592@gmit.ie>
2019-02-05 14:35:07 +0000

    rotten poatatoes on local

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,9 +13,9 @@ def sum(arr)
 end
 
 # my test cases
-raise 'sum([]) != 0' unless sum([]) == 0
-raise 'sum([1]) != 1' unless sum([1]) == 1
-raise 'sum([1,2,3]) != 6' unless sum([1,2,3]) == 6
+# raise 'sum([]) != 0' unless sum([]) == 0
+# raise 'sum([1]) != 1' unless sum([1]) == 1
+# raise 'sum([1,2,3]) != 6' unless sum([1,2,3]) == 6
 
 # Define a method max_2_sum(array) which takes an array of integers as an argument
 # and returns the sum of its two largest elements. For an empty array it should return zero.
@@ -27,28 +27,29 @@ def max_2_sum(arr)
 end
 
 # my test cases
-raise 'max_2_sumum([]) != 0' unless max_2_sum([]) == 0
-raise 'max_2_sum([9]) != 9' unless max_2_sum([9]) == 9
-raise 'max_2_sum([1,2,3]) != 5' unless max_2_sum([1,2,3]) == 5
-raise 'max_2_sum([5,4,2,10]) != 15' unless max_2_sum([5,4,2,10]) == 15
+# raise 'max_2_sumum([]) != 0' unless max_2_sum([]) == 0
+# raise 'max_2_sum([9]) != 9' unless max_2_sum([9]) == 9
+# raise 'max_2_sum([1,2,3]) != 5' unless max_2_sum([1,2,3]) == 5
+# raise 'max_2_sum([5,4,2,10]) != 15' unless max_2_sum([5,4,2,10]) == 15
 
 # Define a method sum_to_n?(array, n) that takes an array of integers and an additional integer,
 # n, as arguments and returns true if any two elements in the array of integers sum to n. sum_to_n?([], n)
 # should return false for any value of n, by definition. Run associated tests via: $ rspec -e '#sum_to_n' spec/part1_spec.rb
 def sum_to_n?(arr, n)
-  return false if arr.empty?
+  return false if arr.empty? or arr.length == 1
   # returns true at first pair that sum to n
   return true if arr.combination(2).find { |x, y| x + y == n }
   # returns true at first pair that sum to n
+  return false
 end
 
 # my test cases
-raise 'sum_to_n?([],0) != 0' unless sum_to_n?([], 0) == false
-raise 'sum_to_n?([2,2],4) != 0' unless sum_to_n?([2,2],4) == true
-raise 'sum_to_n?([2,6,2,5],4) != 0' unless sum_to_n?([2,6,2,5],4) == true
+# raise 'sum_to_n?([],0) != 0' unless sum_to_n?([], 0) == false
+# raise 'sum_to_n?([2,2],4) != 0' unless sum_to_n?([2,2],4) == true
+# raise 'sum_to_n?([2,6,2,5],4) != 0' unless sum_to_n?([2,6,2,5],4) == true
 
 # Part 2
-
+# rspec spec/part2_spec.rb
 def hello(name)
   "Hello, #{name}"
 end
@@ -63,7 +64,20 @@ def binary_multiple_of_4?(s)
 end
 
 # Part 3
-
+# rspec -e 'getters and setters' spec/part3_spec.rb
+# rspec -e '#price_as_string' spec/part3_spec.rb
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn
+	attr_accessor :price
+	
+	def initialize(isbn, price)
+		raise ArgumentError, 
+			"Illegal argument" if isbn.empty?  or price <= 0
+		@isbn = isbn
+		@price = price
+	end
+	
+	def price_as_string
+	  sprintf("$%2.2f", @price)
+	end
 end
